4b80c89e1bfa57f40720a48e201485da0894ccf2
darshan.cometchat <darshan.bhanushali@inscripts.com>
2022-02-09 17:00:03 +0530

    Android UI Kit v2.4.2-1

--- app/src/main/java/com/cometchat/pro/androiduikit/constants/AppConfig.kt
+++ app/src/main/java/com/cometchat/pro/androiduikit/constants/AppConfig.kt
@@ -2,8 +2,8 @@ package com.cometchat.pro.androiduikit.constants
 
 class AppConfig {
     object AppDetails {
-        const val APP_ID = "16932cdb926f3b3"
-        const val AUTH_KEY = "cb6c9faacf94635e68f8788fa0738131b09cf383"
-        const val REGION = "eu"
+        const val APP_ID = "XXXXXXXXXXXXXXXXXXXX"
+        const val AUTH_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXX"
+        const val REGION = "XX"
     }
 }
